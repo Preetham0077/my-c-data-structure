229093dcf847c33898a2515732fdf8f78ccd6735
Preetham0077 <preethampreetu247@gmail.com>
2025-11-03 11:55:29 +0530

    Create calloc.c

---
+++ calloc.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int*arr;
+    arr=(int*)calloc(5,sizeof(int));
+    if(arr==NULL){
+        printf("memory not allocated\n");
+    }
+    printf("memory allocated\n");
+}
